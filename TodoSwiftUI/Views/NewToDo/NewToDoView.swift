27f1f02d23a8447cf1131b6e5a285cfdb60a4268
Laura <lauracorssac@gmail.com>
2020-01-16 21:36:39 -0300

    adding classes for new to do

---
+++ TodoSwiftUI/Views/NewToDo/NewToDoView.swift
@@ -0,0 +1,21 @@
+//
+//  NewToDoView.swift
+//  TodoSwiftUI
+//
+//  Created by Laura Corssac on 1/16/20.
+//  Copyright © 2020 Laura Corssac. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewToDoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewToDoView()
+    }
+}
---
+++ TodoSwiftUI/Views/NewToDo/NewToDoViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewToDoViewModel.swift
+//  TodoSwiftUI
+//
+//  Created by Laura Corssac on 1/16/20.
+//  Copyright © 2020 Laura Corssac. All rights reserved.
+//
+
+import Foundation
